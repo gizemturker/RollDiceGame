5e869e50bbece5836aed6d05f08525687da96a06
Gizem Turker <gzm.aiesec@gmail.com>
2022-03-31 10:52:17 +0300

    shake added. function fixed

--- RollDiceGame/ViewController.swift
+++ RollDiceGame/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
     
     
     
+  
     @IBOutlet weak var lblOyuncu1Skor: UILabel!
     @IBOutlet weak var lblOyuncu2Skor: UILabel!
     @IBOutlet weak var lblOyuncu1Puan: UILabel!
@@ -21,12 +22,81 @@ class ViewController: UIViewController {
     @IBOutlet weak var imgZar2: UIImageView!
     @IBOutlet weak var lblSetSonucu: UILabel!
     
+    
+    var oyuncuPuanlari = (birinciOyuncuPuani : 0 , ikinciOyuncuPuani : 0)
+    var oyuncuSkorlari = (birinciOyuncuSkoru : 0 , ikinciOyuncuSkoru : 0)
+    var oyuncuSira : Int = 1
+    
+    
+    var maxSetSayisi : Int = 5
+    var suankiSet : Int = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if let arkaPlan = UIImage(named: "arkaPlan") {
             self.view.backgroundColor = UIColor(patternImage: arkaPlan)
         }
+        
+        
     }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        
+        zarDegerleriniUret()
+    }
+    func setSonucu(zar1 : Int , zar2 : Int) {
+        
+        if oyuncuSira == 1 {
+            
+            oyuncuPuanlari.birinciOyuncuPuani = zar1 + zar2
+            lblOyuncu1Puan.text = String(oyuncuPuanlari.birinciOyuncuPuani)
+            imgOyuncu1Durum.image = UIImage(named: "bekle")
+            imgOyuncu2Durum.image = UIImage(named: "onay")
+            lblSetSonucu.text = "Sıra 2. oyuncuda!"
+            oyuncuSira = 2
+            lblOyuncu2Puan.text = "0"
+            
+        } else {
+            oyuncuPuanlari.ikinciOyuncuPuani = zar1 + zar2
+            lblOyuncu2Puan.text = String(oyuncuPuanlari.ikinciOyuncuPuani)
+            imgOyuncu1Durum.image = UIImage(named: "onay")
+            imgOyuncu2Durum.image = UIImage(named: "bekle")
+            oyuncuSira = 1
+            
+            if oyuncuPuanlari.birinciOyuncuPuani > oyuncuPuanlari.ikinciOyuncuPuani {
+                
+                oyuncuSkorlari.birinciOyuncuSkoru += 1
+                lblSetSonucu.text = "\(suankiSet). Seti 1. oyuncu kazandı!"
+                
+                suankiSet += 1
+                lblOyuncu1Skor.text = String(oyuncuSkorlari.birinciOyuncuSkoru)
+            } else if oyuncuPuanlari.ikinciOyuncuPuani > oyuncuPuanlari.birinciOyuncuPuani {
+                
+                oyuncuSkorlari.ikinciOyuncuSkoru += 1
+                lblSetSonucu.text = "\(suankiSet). Seti 2. oyuncu kazandı!"
+                suankiSet += 1
+                lblOyuncu2Skor.text = String(oyuncuSkorlari.ikinciOyuncuSkoru)
+            } else {
+                lblSetSonucu.text = "\(suankiSet). Set berabere kaldı."
+            }
+            
+            oyuncuPuanlari.birinciOyuncuPuani = 0
+            oyuncuPuanlari.ikinciOyuncuPuani = 0
+        }
+        
+    }
+    func zarDegerleriniUret() {
+        
+        let zar1 = arc4random_uniform(6) + 1
+        let zar2 = arc4random_uniform(6) + 1
+        
+        imgZar1.image = UIImage(named: String(zar1))
+        imgZar2.image = UIImage(named: String(zar2))
+        
+        setSonucu(zar1: Int(zar1) , zar2: Int(zar2))
+        
+    }
+    
 }
 
